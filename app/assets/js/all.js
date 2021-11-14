988352ecb14c2090fabbb5569f6c6d6d912330a7
Erin Huang <erin.huang09@gmail.com>
2021-11-14 23:32:57 +0800

    index and attractions update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,35 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// slick套件載入不成功
+$('.slick-body').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  autoplay: true,
+  arrows: true,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      setting: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 4000,
+      }
+    },
+    {
+      breakpoint: 480,
+      setting: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 4000,
+      }
+    }
+  ]
 });
